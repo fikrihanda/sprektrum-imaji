fc37c9d0621daa3936ebd7eabf65e3061f86e082
Fikri Handa <fikrihanda@gmail.com>
2025-03-20 04:39:35 +0700

    services

---
+++ components/services/how.vue
@@ -0,0 +1,52 @@
+<script lang="ts" setup>
+const howDoit = computed(() => {
+  return [
+    {
+      title: 'Initial Consultation',
+      desc: 'Schedule an initial meeting to discuss your goals and social media needs.',
+    },
+    {
+      title: 'Strategy Development',
+      desc: 'We\'ll create a customized social media strategy tailored to your brand.',
+    },
+    {
+      title: 'Launch and Improve',
+      desc: 'We implement the strategy and monitor performance for results.',
+    },
+    {
+      title: 'Launch and Improve',
+      desc: 'We implement the strategy and monitor performance for results.',
+    },
+  ]
+})
+</script>
+
+<template>
+  <div class="services-how">
+    <div class="home-entitled-group">
+      <div class="home-entitled-container !w-full">
+        <div class="home-entitled-title">
+          How do we do it?
+        </div>
+        <div class="home-entitled-subtitle">
+          Easy to begin and streamlined to the end. We'll stick to async comms and keep meetings to a minimum, for your sake and mine.
+        </div>
+      </div>
+    </div>
+    <div class="services-how-items">
+      <div v-for="(item, i) in howDoit" :key="`how-${i}`" class="services-how-item">
+        <div class="">
+          <div class="services-how-number">
+            0{{ i + 1 }}
+          </div>
+        </div>
+        <div class="services-how-title">
+          {{ item.title }}
+        </div>
+        <div class="services-how-desc">
+          {{ item.desc }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ components/services/our.vue
@@ -0,0 +1,58 @@
+<script lang="ts" setup>
+const servicesItems = computed(() => {
+  return [
+    {
+      icon: 'medal',
+      title: 'Landing Page Design',
+    },
+    {
+      icon: 'monitor',
+      title: 'UI/UX Design',
+    },
+    {
+      icon: 'monitor',
+      title: 'UI/UX Design',
+    },
+    {
+      icon: 'community',
+      title: 'Web Development',
+    },
+    {
+      icon: 'megaphone',
+      title: 'App Development',
+    },
+    {
+      icon: 'megaphone',
+      title: 'App Development',
+    },
+  ]
+})
+</script>
+
+<template>
+  <div class="services-our ">
+    <div class="home-entitled-group">
+      <div class="home-entitled-container !w-full">
+        <div class="home-entitled-title">
+          Our Services
+        </div>
+        <div class="home-entitled-subtitle">
+          We offer tailored solutions to enhance your social media presence and drive real results for your business.
+        </div>
+      </div>
+    </div>
+    <div class="services-our-items">
+      <div v-for="(item, i) in servicesItems" :key="`services-${i}`" class="services-our-item">
+        <div>
+          <NuxtIcon :name="`my-icon:${item.icon}`" size="32" class="text-secondary" />
+        </div>
+        <div class="services-our-item-title">
+          {{ item.title }}
+        </div>
+        <div class="services-our-item-desc">
+          We keep an eye on what people are saying about your brand online. This helps us manage your reputation.
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
