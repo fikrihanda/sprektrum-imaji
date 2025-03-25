a6c9cba56162cac4a2c32ceb98e1e19ea51c9068
Fikri Handa <fikrihanda@gmail.com>
2025-03-25 14:33:48 +0700

    foot

--- components/foot/about.vue
+++ components/foot/about.vue
@@ -5,43 +5,54 @@
 <template>
   <div class="foot-about">
     <div class="foot-about-content">
-      <div class="foot-about-item">
-        <img src="~/assets/images/logo.svg" class="h-[40px]">
-        <div class="mt-[16px] foot-about-boost">
-          Boost Your Brand with Social Media Marketing
+      <div class="w-[600px]">
+        <img src="~/assets/images/logo.svg" class="w-[200px]">
+        <div class="mt-[32px] foot-about-boost">
+          We believe in the power of design to transform businesses. Our team of talented designers, strategists, and creative thinkers.
         </div>
       </div>
-      <div class="foot-about-item">
-        <div class="foot-about-item-title">
-          Company
+      <div class="flex items-center flex-wrap">
+        <div class="foot-about-item w-[180px]">
+          <div class="foot-about-item-title">
+            Company
+          </div>
+          <div class="normalize">
+            About
+          </div>
+          <div class="normalize">
+            Career
+          </div>
+          <div class="normalize">
+            Become Affiliate
+          </div>
         </div>
-        <div class="normalize">
-          About
+        <div class="foot-about-item w-[180px]">
+          <div class="foot-about-item-title">
+            Sosial Media
+          </div>
+          <div class="normalize">
+            Instagram
+          </div>
+          <div class="normalize">
+            Linkedin
+          </div>
+          <div class="normalize">
+            Whatsapp
+          </div>
         </div>
-        <div class="normalize">
-          Article
-        </div>
-      </div>
-      <div class="foot-about-item">
-        <div class="foot-about-item-title">
-          Sosial Media
-        </div>
-        <div class="normalize">
-          Instagram
-        </div>
-        <div class="normalize">
-          Linkedit
-        </div>
-      </div>
-      <div class="foot-about-item">
-        <div class="foot-about-item-title">
-          Contact
-        </div>
-        <div class="normalize">
-          Contact Us
-        </div>
-        <div class="normalize">
-          spektrum.imaji@email.com
+        <div class="foot-about-item w-[180px]">
+          <div class="foot-about-item-title">
+            Contact
+          </div>
+          <div class="normalize">
+            (62) 1829017
+          </div>
+          <div class="normalize">
+            hello@spektrum.com
+          </div>
+          <div class="normalize">
+            Bogor, Jawa Barat
+          </div>
         </div>
       </div>
     </div>
