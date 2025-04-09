e8def4d9dc675d515eb52581156419d6e936d838
Fikri Handa <fikrihanda@gmail.com>
2025-04-09 14:58:12 +0700

    menu responsive

--- components/header.vue
+++ components/header.vue
@@ -1,6 +1,31 @@
 <script lang="ts" setup>
 const refEl = ref<HTMLElement>()
+const mdContent = ref<HTMLElement>()
 const refHeight = ref<number>()
+const toMenu = ref('#menu-lg')
+const toIcon = ref('#icon-lg')
+const mounted = ref(false)
+const open = ref(false)
+
+const listsLang = computed(() => {
+  return [
+    {
+      id: 'en',
+      icon: 'flag-eng',
+      name: 'English',
+    },
+    {
+      id: 'id',
+      icon: 'flag-idn',
+      name: 'Indonesia',
+    },
+  ]
+})
+const activeBreakpoint = breakpoints.active()
+
+const selectedLang = ref<typeof listsLang.value[0]>(
+  listsLang.value[0],
+)
 
 function scrollHeader() {
   const lebihScroll = window.scrollY > (refHeight.value! + 60)
@@ -14,33 +39,108 @@ function scrollHeader() {
   }
 }
 
+function setMenu() {
+  if (breakpoints.isGreater('md')) {
+    toMenu.value = '#menu-lg'
+    toIcon.value = '#icon-lg'
+  }
+  else {
+    toMenu.value = '#menu-md'
+    toIcon.value = '#icon-md'
+  }
+}
+
+function resize() {
+  if (breakpoints.isGreater('md') && open.value) {
+    open.value = false
+  }
+}
+
+watch(open, (val) => {
+  if (val) {
+    document.body.classList.add('si-header-no-overflow')
+  }
+  else {
+    document.body.classList.remove('si-header-no-overflow')
+  }
+})
+
 onMounted(() => {
+  mounted.value = true
+  setMenu()
   refHeight.value = refEl.value?.offsetHeight
   scrollHeader()
+  onClickOutside(mdContent, () => {
+    open.value = false
+  })
   window.addEventListener('scroll', scrollHeader)
+  window.addEventListener('resize', resize)
 })
+
+onBeforeMount(() => {
+  window.removeEventListener('scroll', scrollHeader)
+  window.removeEventListener('resize', resize)
+})
+
+watch(activeBreakpoint, () => setMenu())
 </script>
 
 <template>
   <div ref="refEl" class="si-header">
     <img src="~/assets/images/logo.svg">
-    <div class="si-header-menu">
-      <NuxtLink to="/">
-        Home
-      </NuxtLink>
-      <NuxtLink to="/about">
-        About
-      </NuxtLink>
-      <NuxtLink to="/services">
-        Services
-      </NuxtLink>
-      <NuxtLink to="/works">
-        Works
-      </NuxtLink>
-      <NuxtLink to="/contact">
-        Contact
-      </NuxtLink>
-    </div>
-    <!-- <div>a</div> -->
+    <div id="menu-lg" class="si-header-menu" />
+    <div id="icon-lg" class="si-header-opt" />
+    <button class="si-header-bars" @click="open = true">
+      <NuxtIcon name="my-icon:bars" size="16" />
+    </button>
   </div>
+  <div class="si-header-md" :class="[open && 'open']">
+    <div class="si-header-md-header">
+      <img src="~/assets/images/logo.svg">
+      <button class="si-header-bars" @click="open = false">
+        <NuxtIcon name="my-icon:close" size="16" />
+      </button>
+    </div>
+    <div ref="mdContent" class="si-header-md-content">
+      <div id="menu-md" />
+      <div id="icon-md" />
+    </div>
+  </div>
+  <Teleport to="body" :disabled="!open">
+    <div class="si-header-backdrop" :class="[!open && 'hidden']" />
+  </Teleport>
+  <Teleport v-if="mounted" :to="toMenu">
+    <NuxtLink to="/">
+      Home
+    </NuxtLink>
+    <NuxtLink to="/about">
+      About
+    </NuxtLink>
+    <NuxtLink to="/services">
+      Services
+    </NuxtLink>
+    <NuxtLink to="/works">
+      Works
+    </NuxtLink>
+    <NuxtLink to="/contact">
+      Contact
+    </NuxtLink>
+  </Teleport>
+  <Teleport v-if="mounted" :to="toIcon">
+    <div class="si-header-opt-icon">
+      <a href="#" class="si-header-opt-icon-item">
+        <NuxtIcon name="my-icon:instagram" />
+      </a>
+      <a href="#" class="si-header-opt-icon-item">
+        <NuxtIcon name="my-icon:linkedin" />
+      </a>
+    </div>
+    <div class="si-header-dd">
+      <button class="si-header-dd-btn">
+        <NuxtIcon :name="`my-icon:${selectedLang.icon}`" class="mr-[8px]" />
+        {{ selectedLang.name }}
+        <NuxtIcon name="my-icon:chevron-down" />
+      </button>
+    </div>
+  </Teleport>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,5 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
@@ -34,6 +36,14 @@ export default defineNuxtConfig({
       theme: {
         fontFamily: {
           sans: ['"Space Grotesk"', 'Tofu'],
+        },
+        screens: {
+          ...defaultTheme.screens,
+          'sm': '641px',
+          'md': '769px',
+          'lg': '1025px',
+          'xl': '1281px',
+          '2xl': '1536px',
         },
         extend: {
           colors: {
